56e6b28d21bfcfa50d48fdbd8d0b019cd750821c
hun52 <hongsh05@gmail.com>
2024-08-10 15:15:59 +0900

    [API-1] Create git repository
    - Setup default setup for api
    - Need to cover CMakeLists.txt to make a shared library.
      - Need to add application level

---
+++ library/api.cpp
@@ -0,0 +1,114 @@
+#include <api.h>
+#include <iostream>
+
+using namespace math_api;
+
+api_error_t add(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else {
+        int64_t const check_overflow {static_cast<int64_t>(a) + static_cast<int64_t>(b)};
+        if(check_overflow > INT32_MAX || check_overflow < INT32_MIN) {
+            std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Overflow occurs from given param" << std::endl;
+            err = API_ERROR_OVERFLOW;
+        } 
+        else {
+            *result = static_cast<int32_t>(check_overflow);
+        }
+    }
+    return err;
+}
+
+api_error_t sub(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else {
+        int64_t const check_overflow {static_cast<int64_t>(a) - static_cast<int64_t>(b)};
+        if(check_overflow > INT32_MAX || check_overflow < INT32_MIN) {
+            std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Overflow occurs from given param" << std::endl;
+            err = API_ERROR_OVERFLOW;
+        } 
+        else {
+            *result = static_cast<int32_t>(check_overflow);
+        }
+    }
+    return err;
+}
+
+api_error_t mul(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else {
+        int64_t const check_overflow {static_cast<int64_t>(a) * static_cast<int64_t>(b)};
+        if(check_overflow > INT32_MAX || check_overflow < INT32_MIN) {
+            std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Overflow occurs from given param" << std::endl;
+            err = API_ERROR_OVERFLOW;
+        } 
+        else {
+            *result = static_cast<int32_t>(check_overflow);
+        }
+    }
+    return err;
+}
+
+api_error_t div(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else if(b == 0) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Division by zero occurs" << std::endl;
+        err = API_ERROR_DIVISION_BY_ZERO;
+    } 
+    else {
+        *result = a / b;
+    }
+    return err;
+}
+
+api_error_t mod(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else if(b == 0) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Division by zero occurs" << std::endl;
+        err = API_ERROR_DIVISION_BY_ZERO;
+    } 
+    else {
+        *result = a % b;
+    }
+    return err;
+}
+
+api_error_t pow(int32_t* const result, int32_t const a, int32_t const b) {
+    api_error_t err { API_SUCCESS };
+    if(result == nullptr) {
+        std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Invalid parameter is given" << std::endl;
+        err = API_ERROR_INVALID_PARAMETER;
+    } 
+    else {
+        int64_t check_overflow {1};
+        for(int32_t i {}; i < b; ++i) {
+            check_overflow *= a;
+            if(check_overflow > INT32_MAX || check_overflow < INT32_MIN) {
+                std::cerr << "__FILE__" << ":" << __LINE__ << " " << __func__ << " " << "Overflow occurs from given param" << std::endl;
+                err = API_ERROR_OVERFLOW;
+                break;
+            }
+        }
+        *result = static_cast<int32_t>(check_overflow);
+    }
+    return err;
+}
